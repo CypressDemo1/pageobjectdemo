420fb20ee03f9f03d93208d61bb813348cec778f
markttaylor <mark.taylor@monster.com>
2022-09-25 17:45:57 +1000

    Initial page objects

--- cypress/e2e/test1.cy.ts
+++ cypress/e2e/test1.cy.ts
@@ -6,23 +6,41 @@ describe('example to-do app', () => {
       cy.viewport(1536, 960)
     })
   
-    it('Test header navigation', () => {
-        let home=new HomePage()
-        home
-        .header.toHome()
-        .header.toBrowse()
-        .header.toSearch()
-        .header.tocreateAccount()
-        .header.toHelp()
-        .header.toSignIn()
-    })
+    // it('Test header navigation', () => {
+    //     let home=new HomePage()
+    //     home
+    //     .header.toHome()
+    //     .header.toBrowse()
+    //     .header.toSearch()
+    //     .header.tocreateAccount()
+    //     .header.toHelp()
+    //     .header.toSignIn()
+    // })
   
-    it('Test footer navigation', () => {
+    // it('Test footer navigation', () => {
+    //     let home=new HomePage()
+    //     home
+    //     .footer.toAbout()
+    //     .footer.toHelp()
+    //     .footer.toContact()
+    //     // .footer.toFeedback()
+    // })
+  
+    // it('Test top navigator', () => {
+    //     let home=new HomePage()
+    //     home
+    //     .header.toBrowse()
+    //     .topNav.toRaisingKids()
+    //     .topNav.toLivingArrangements()
+    //     .topNav.toAgeing()
+    //     .topNav.toWork()
+    //     .topNav.toEducation()
+    //     .topNav.toHealthAndDisability()
+    // })
+  
+    it('Search for accessibility', () => {
         let home=new HomePage()
         home
-        .footer.toAbout()
-        .footer.toHelp()
-        .footer.toContact()
-        .footer.toFeedback()
+        .header.toSearch()
     })
 })
